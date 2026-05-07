4ed27c857891fc86244184f9e84ef3fa5deaa4e5
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-05-06 22:51:28 -0500

    configure register schema with zod and use it in register form. Show errors managed by zod.

--- src/components/pages/registerPage/RegisterPage.tsx
+++ src/components/pages/registerPage/RegisterPage.tsx
@@ -1,10 +1,19 @@
-
 import { ChartNoAxesCombined, ShieldCheck } from 'lucide-react';
 import logo from '../../../assets/images/monos-track-logo-circle.png'
 import { cn } from '../../../utils/cn';
 import LoginButton from '../../../features/auth/components/LoginButton';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { RegisterSchema, type RegisterData } from '../../../features/auth/schemas/registerSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 function RegisterPage() {
+  const { register, handleSubmit, formState: { errors } } = useForm<RegisterData>({
+    resolver: zodResolver(RegisterSchema)
+  })
+  const onSubmit: SubmitHandler<RegisterData> = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <section className={cn('w-full p-8 flex flex-col justify-center items-center gap-6')}>
@@ -23,67 +32,102 @@ function RegisterPage() {
             <strong className='font-bold text-2xl text-primary-dark dark:text-neutral-dark md:text-4xl'>Monos Track</strong>
           </div>
         </div>
-        <form className={cn(
-          'w-full grid grid-cols-2 gap-3',
-        )}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={cn(
+            'w-full p-6 grid grid-cols-2 gap-3 inset-shadow-sm inset-shadow-neutral-light rounded-xl',
+          )}
+        >
           <div className={cn('flex flex-col justify-center items-start col-span-2')}>
             <label htmlFor="firstName">Nombre(s)</label>
             <input
               type="text"
-              name="firstName"
               id="firstName"
               placeholder="Alejandro"
-              className={cn('w-full px-4 py-2.5 border-2 border-primary-dark rounded-lg')}
+              className={cn('w-full px-4 py-2.5 border-2 border-secondary-light/70 rounded-lg')}
+              {...register('firstName')}
             />
+            {errors.firstName && (
+              <p className='text-sm text-red-500 mt-1'>
+                {errors.firstName.message}
+              </p>
+            )}
           </div>
           <div className={cn('flex flex-col justify-center items-start col-span-2')}>
             <label htmlFor="lastName">Apellido(s)</label>
             <input
               type="text"
-              name="lastName"
               id="lastName"
               placeholder="Martínez"
-              className={cn('w-full px-4 py-2.5 border-2 border-primary-dark rounded-lg')}
+              className={cn('w-full px-4 py-2.5 border-2 border-secondary-light/70 rounded-lg')}
+              {...register('lastName')}
             />
+            {errors.lastName && (
+              <p className='text-sm text-red-500 mt-1'>
+                {errors.lastName.message}
+              </p>
+            )}
           </div>
           <div className={cn('flex flex-col justify-center items-start col-span-2')}>
             <label htmlFor="email">Correo electrónico</label>
             <input
               type="email"
-              name="email"
               id="email"
               placeholder="alejandro_martinez@email.com"
-              className={cn('w-full px-4 py-2.5 border-2 border-primary-dark rounded-lg')}
+              className={cn('w-full px-4 py-2.5 border-2 border-secondary-light/70 rounded-lg')}
+              {...register('email')}
             />
+            {errors.email && (
+              <p className='text-sm text-red-500 mt-1'>
+                {errors.email.message}
+              </p>
+            )}
           </div>
           <div className={cn('flex flex-col justify-center items-start col-span-2')}>
             <label htmlFor="password">Contraseña</label>
             <input
               type="password"
-              name="password"
               id="password"
               placeholder="**********"
-              className={cn('w-full px-4 py-2.5 border-2 border-primary-dark rounded-lg')}
+              className={cn('w-full px-4 py-2.5 border-2 border-secondary-light/70 rounded-lg')}
+              {...register('password')}
             />
+            {errors.password && (
+              <p className='text-sm text-red-500 mt-1'>
+                {errors.password.message}
+              </p>
+            )}
           </div>
           <div className={cn('flex flex-col justify-center items-start col-span-2')}>
             <label htmlFor="confirmPassword">Confirmar contraseña</label>
             <input
               type="password"
-              name="confirmPassword"
               id="confirmPassword"
               placeholder="**********"
-              className={cn('w-full px-4 py-2.5 border-2 border-primary-dark rounded-lg')}
+              className={cn('w-full px-4 py-2.5 border-2 border-secondary-light/70 rounded-lg')}
+              {...register('confirmPassword')}
             />
+            {errors.confirmPassword && (
+              <p className='text-sm text-red-500 mt-1'>
+                {errors.confirmPassword.message}
+              </p>
+            )}
           </div>
-          <div className={cn('flex justify-center items-center gap-2 col-span-2')}>
-            <input
-              type="checkbox"
-              name="termsAndConditions"
-              id="termsAndConditions"
-              className={cn('transform scale-150 align-middle')}
-            />
-            <label htmlFor="termsAndConditions">Acepto los Términos de Servicio y la Política de Privacidad de Monos Track.</label>
+          <div className={cn('w-full flex flex-col justify-center items-start col-span-2')}>
+            <div className={cn('flex justify-center items-center gap-4')}>
+              <input
+                type="checkbox"
+                id="termsAndConditions"
+                className={cn('transform scale-150 align-middle')}
+                {...register('termsAndConditions')}
+              />
+              <label htmlFor="termsAndConditions">Acepto los Términos de Servicio y la Política de Privacidad de Monos Track.</label>
+            </div>
+            {errors.termsAndConditions && (
+              <p className='text-sm text-red-500 mt-1'>
+                {errors.termsAndConditions.message}
+              </p>
+            )}
           </div>
           <button
             type="submit"
--- src/features/auth/schemas/registerSchema.ts
+++ src/features/auth/schemas/registerSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 // Regular expression for: 1 uppercase letter, 1 number and 1 special character
 const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}[\]:;"'<>,.?/\\|`~]).*$/;
 
-export const registerSchema = z.object({
+export const RegisterSchema = z.object({
   firstName: z
     .string()
     .min(1, { message: "El nombre es obligatorio" })
@@ -16,21 +16,23 @@ export const registerSchema = z.object({
     .string()
     .min(1, { message: "El correo es obligatorio" })
     .email({ message: "Formato de correo inválido" }),
-
   password: z
     .string()
     .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
     .regex(passwordRegex, {
       message: "La contraseña debe incluir al menos una mayúscula, un número y un carácter especial",
     }),
-
   confirmPassword: z
     .string()
     .min(1, { message: "Debes confirmar tu contraseña" }),
+  termsAndConditions: z
+    .literal(true, {
+      error: () => ({ message: "Debes aceptar los térmimos y condiciones." }),
+    }),
 })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Las contraseñas no coinciden",
     path: ["confirmPassword"], // The error will be associated with the confirmPassword field
   });
 
-export type RegisterInput = z.infer<typeof registerSchema>;
+export type RegisterData = z.infer<typeof RegisterSchema>;
