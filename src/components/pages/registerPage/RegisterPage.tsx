7598a00f6bfaa894ded77b759cfda796f3e886da
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-05-10 20:51:16 -0500

    style dark mode in desktop query

--- src/components/pages/registerPage/RegisterPage.tsx
+++ src/components/pages/registerPage/RegisterPage.tsx
@@ -15,13 +15,13 @@ function RegisterPage() {
   }
 
   return (
-    <div className={cn('w-full min-h-screen flex flex-row-reverse justify-center items-center')}>
-      <section className={cn('w-full min-h-screen p-8 flex flex-col justify-center items-center gap-4 dark:bg-primary-dark dark:shadow-xl/50 dark:shadow-neutral-light lg:w-1/2')}>
+    <div className={cn('w-full flex flex-row-reverse')}>
+      <section className={cn('w-full h-full min-h-screen p-8 flex flex-col justify-center items-center gap-4 dark:bg-primary-dark dark:shadow-xl/50 dark:shadow-neutral-light lg:w-1/2')}>
         <header className={cn('flex flex-col-reverse justify-center items-center gap-4 md:max-w-120')}>
           <div>
             <h1 className={cn('sr-only')}>Registro</h1>
             <strong className={cn('font-semibold dark:text-neutral-dark md:block md:text-center md:text-xl')}>Comienza ahora</strong>
-            <p className={cn('text-gray-text dark:text-neutral-light md:text-center md:mt-4')}>Registrate para gestionar tus movimientos con precisión técnica.</p>
+            <p className={cn('text-gray-text text-pretty dark:text-neutral-light md:text-center md:mt-4')}>Registrate para gestionar tus movimientos con precisión técnica.</p>
           </div>
           <div className={cn('flex justify-center items-center gap-1 lg:hidden')}>
             <img
@@ -98,7 +98,7 @@ function RegisterPage() {
               </p>
             )}
           </div>
-          <div className={cn('flex flex-col justify-center items-start gap-0.5 col-span-2')}>
+          <div className={cn('flex flex-col justify-center items-start gap-0.5 col-span-2 xl:col-span-1')}>
             <label
               htmlFor="password"
               className='dark:text-neutral-dark'
@@ -118,7 +118,7 @@ function RegisterPage() {
               </p>
             )}
           </div>
-          <div className={cn('flex flex-col justify-center items-start gap-0.5 col-span-2')}>
+          <div className={cn('flex flex-col justify-center items-start gap-0.5 col-span-2 xl:col-span-1')}>
             <label
               htmlFor="confirmPassword"
               className='dark:text-neutral-dark'
@@ -175,7 +175,7 @@ function RegisterPage() {
           />
         </footer>
       </section>
-      <section className={cn('hidden h-full lg:w-1/2 lg:p-12 lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-4 lg:bg-linear-180 lg:from-primary/70 lg:to-primary/20')}>
+      <section className={cn('hidden h-full min-h-screen lg:w-1/2 lg:p-12 lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-4 lg:bg-linear-180 lg:from-primary/70 lg:to-primary/20 dark:bg-linear-120 dark:from-tertiary-dark dark:to-primary-dark')}>
         <div className={cn('flex flex-col justify-center items-center gap-2')}>
           <img
             src={logo}
@@ -184,22 +184,22 @@ function RegisterPage() {
           />
           <strong className='font-bold text-2xl text-primary-dark dark:text-neutral-dark md:text-4xl'>Monos Track</strong>
         </div>
-        <strong>Gestión finaciera de alto nivel.</strong>
-        <p className={cn('text-center text-pretty')}>Únete a la plataforma preferida por analístas para el seguimiento preciso de flujos de capital y activos digitales.</p>
-        <div className={cn('flex justify-center items-center gap-2')}>
-          <article className={cn('p-6 bg-neutral-light/40 rounded-xl')}>
+        <strong className={cn('dark:text-neutral-dark/80')}>Gestión finaciera de alto nivel.</strong>
+        <p className={cn('text-center text-pretty dark:text-neutral-dark/65')}>Únete a la plataforma preferida por analístas para el seguimiento preciso de flujos de capital y activos digitales.</p>
+        <div className={cn('flex justify-center items-center gap-3')}>
+          <article className={cn('p-6 bg-neutral-light/40 rounded-xl dark:bg-primary-dark/60 dark:shadow-2xs dark:shadow-neutral-dark/50')}>
             <div className={cn('flex flex-col-reverse justify-center items-start gap-2')}>
-              <h3 className={cn('text-lg font-semibold')}>Seguridad bancaria</h3>
-              <ShieldCheck />
+              <h3 className={cn('text-lg font-semibold dark:text-neutral-dark/90')}>Seguridad bancaria</h3>
+              <ShieldCheck size={30} className={cn('dark:text-neutral-light')} />
             </div>
-            <p>Tus datos están encriptados con los más altos estándares.</p>
+            <p className={cn('dark:text-neutral-dark/75')}>Tus datos están encriptados con los más altos estándares.</p>
           </article>
-          <article className={cn('p-6 bg-neutral-light/40 rounded-xl')}>
+          <article className={cn('p-6 bg-neutral-light/40 rounded-xl dark:bg-primary-dark/60 dark:shadow-2xs dark:shadow-neutral-dark/50')}>
             <div className={cn('flex flex-col-reverse justify-center items-start gap-2')}>
-              <h3 className={cn('text-lg font-semibold')}>Análisis inteligente</h3>
-              <ChartNoAxesCombined />
+              <h3 className={cn('text-lg font-semibold dark:text-neutral-dark/90')}>Análisis inteligente</h3>
+              <ChartNoAxesCombined size={30} className={cn('dark:text-neutral-light')} />
             </div>
-            <p>Información en tiempo real sobre tus hábitos de gasto.</p>
+            <p className={cn('dark:text-neutral-dark/75')}>Información en tiempo real sobre tus hábitos de gasto.</p>
           </article>
         </div>
       </section>
