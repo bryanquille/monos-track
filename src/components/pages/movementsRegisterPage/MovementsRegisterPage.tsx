d6fac7a4932844ca67258e9699cadbc9084ad039
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-05-26 14:52:50 -0500

    style register movements for tablet and desktop

--- src/components/pages/movementsRegisterPage/MovementsRegisterPage.tsx
+++ src/components/pages/movementsRegisterPage/MovementsRegisterPage.tsx
@@ -7,14 +7,14 @@ function MovementsRegisterPage() {
 
   return (
     <section className={cn('w-[95%] max-w-7xl mx-auto overflow-hidden border border-neutral-light/50 rounded-xl bg-white dark:shadow-[unset] dark:bg-tertiary-dark dark:text-neutral-dark')}>
-      <header className={cn('p-4 flex flex-col justify-center items-center gap-3 bg-neutral-light/50 dark:bg-secondary-light')}>
+      <header className={cn('p-4 flex flex-col justify-center items-center gap-3 bg-neutral-light/50 dark:bg-secondary-light md:p-8 md:flex-row md:justify-evenly')}>
         <div>
-          <h1 className={cn('font-semibold')}>Registrar Movimiento</h1>
+          <h1 className={cn('font-semibold text-xl')}>Registrar Movimiento</h1>
           <p className={cn('text-gray-text dark:text-neutral-dark/70')}>Ingresa los detalles de tu nueva operación financiera.</p>
         </div>
         <button
           type="button"
-          className={cn('relative py-2 px-4 overflow-hidden grid grid-cols-2 gap-2 rounded-xl bg-neutral-light dark:bg-primary-dark dark:border dark:border-neutral-dark')}
+          className={cn('relative cursor-pointer py-2 px-4 overflow-hidden grid grid-cols-2 gap-2 rounded-xl bg-neutral-light dark:bg-primary-dark dark:border dark:border-neutral-dark')}
           onClick={() => setIsIncome(!isIncome)}
         >
           <span
@@ -27,8 +27,8 @@ function MovementsRegisterPage() {
           <span>Gasto</span>
         </button>
       </header>
-      <main className={cn('p-4')}>
-        <form className={cn('flex flex-col justify-center gap-4')}>
+      <main className={cn('p-4 md:p-8')}>
+        <form className={cn('grid grid-cols-1 gap-4 md:grid-cols-2')}>
           <div className={cn('flex flex-col gap-1')}>
             <label htmlFor="amount">Monto</label>
             <input
@@ -67,6 +67,7 @@ function MovementsRegisterPage() {
               <option value="deposit">Deposito</option>
             </select>
           </div>
+          {/* TODO: Make description and file atachment size the same */}
           <div className={cn('flex flex-col gap-1')}>
             <label htmlFor="description">Descripción</label>
             <textarea
@@ -84,26 +85,30 @@ function MovementsRegisterPage() {
           </div>
           <button
             type="submit"
-            className={cn('cursor-pointer p-2 flex justify-center items-center gap-2 rounded-md bg-primary text-white font-semibold text-lg transition-all duration-200 hover:bg-primary/80')}
+            className={cn('cursor-pointer p-2 flex justify-center items-center gap-2 rounded-md bg-primary text-white font-semibold text-lg transition-all duration-200 hover:bg-primary/80 md:py-4 md:col-span-2')}
           >
             Confirmar Movimiento
           </button>
         </form>
       </main>
-      <footer>
-        <article>
+      <footer className={cn('p-4 grid grid-cols-1 grid-rows-2 gap-4 md:p-8 md:grid-cols-2 md:grid-rows-1')}>
+        <article className={cn('w-full p-4 flex flex-row-reverse justify-end items-center gap-4 rounded-xl bg-neutral-light/50 dark:bg-primary-dark')}>
           <div>
-            <h3>Seguridad</h3>
-            <p>Transacción encriptada de extremo a extremo</p>
+            <h3 className={cn('font-semibold')}>Seguridad</h3>
+            <p className={cn('text-gray-text dark:text-neutral-dark/70')}>Transacción encriptada de extremo a extremo</p>
           </div>
-          <ShieldCheck />
+          <div className={cn('p-2 rounded-full bg-neutral-light dark:bg-primary/25')}>
+            <ShieldCheck size={26} className={cn('text-green-600 dark:text-green-500')} />
+          </div>
         </article>
-        <article>
+        <article className={cn('w-full p-4 flex flex-row-reverse justify-end items-center gap-4 rounded-xl bg-neutral-light/50 dark:bg-primary-dark')}>
           <div>
-            <h3>Sincronización</h3>
-            <p>Actualización en tiempo real</p>
+            <h3 className={cn('font-semibold')}>Sincronización</h3>
+            <p className={cn('text-gray-text dark:text-neutral-dark/70')}>Actualización en tiempo real</p>
           </div>
-          <RefreshCcw />
+          <div className={cn('p-2 rounded-full bg-neutral-light dark:bg-primary/25')}>
+            <RefreshCcw size={26} className={cn('text-primary dark:text-secondary-dark')} />
+          </div>
         </article>
       </footer>
     </section>
