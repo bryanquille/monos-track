d5af47ea5faff9c78c126f17530839e8c2c495ee
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-06-24 16:05:41 -0500

    fix color in expense percentage info component

--- src/components/pages/dashboardPage/ExpensePercentageInfo.tsx
+++ src/components/pages/dashboardPage/ExpensePercentageInfo.tsx
@@ -11,7 +11,7 @@ function ExpensePercentageInfo({ category, percentageValue, color }: ExpensePerc
     <div className={cn('flex justify-between items-center')}>
       <div className={cn('flex items-center gap-1.5')}>
         <div
-          className={cn(`w-3 h-3 rounded-4xl bg-[${color}]`)}
+          className={cn(`w-3 h-3 rounded-4xl`)}
           style={{ backgroundColor: color }}
         ></div>
         <p>{category}</p>
