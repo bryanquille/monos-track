80def480dd15d6b7676ad3d914108e816dc8832c
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-08-03 22:21:21 -0500

    feat: add financial data for August 3, 2026 and clean up unused code in dataByYear function

--- src/features/dashboard/mocks/mockup-data.ts
+++ src/features/dashboard/mocks/mockup-data.ts
@@ -89,6 +89,7 @@ export const mockFinancialData = [
   { movement_type: 'income', amount: 1320, movement_date: '2026-08-01' },
   { movement_type: 'expense', amount: 42.28, movement_date: '2026-08-02' },
   { movement_type: 'expense', amount: 215, movement_date: '2026-08-02' },
+  { movement_type: 'expense', amount: 75, movement_date: '2026-08-03' },
 
 ]
 
--- src/features/dashboard/utils/dataByYear.ts
+++ src/features/dashboard/utils/dataByYear.ts
@@ -10,28 +10,6 @@ interface dataByYearTypes {
 }
 
 export const dataByYear = ({ data, year }: dataByYearTypes) => {
-
-  // // Getting the list of available years from all the data
-  // const availableYears = Array.from(new Set(data.map((item) => {
-  //   return item.movement_date.slice(0, 4)
-  // })))
-
-  // // Create an object with the years as a key with an empty array as value
-  // const dataByYears: Record<string, dataTypes[]> = {}
-  // availableYears.forEach(year => { dataByYears[year] = [] })
-
-  // // Fill the array of each year with data according to each year
-  // data.forEach(item => {
-  //   availableYears.forEach(year => {
-  //     if (item.movement_date.slice(0, 4) === year) {
-  //       dataByYears[year].push(item)
-  //     }
-  //   })
-  // })
-
-  // if (!year) return dataByYears
-  // else return dataByYears[year]
-  
   return data.filter(item => {
     return item.movement_date.slice(0, 4) === year
   })
